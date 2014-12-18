881dcf405c0a7fc5b13ae20d4a88972c45789ddc
flow <flowbehappy@gmail.com>
2014-12-18 18:39:06 +0800

    sf - FileSystem is not DistributedFileSystem

--- indexing-hadoop/src/main/java/io/druid/indexer/IndexGeneratorJob.java
+++ indexing-hadoop/src/main/java/io/druid/indexer/IndexGeneratorJob.java
@@ -399,8 +399,7 @@ public class IndexGeneratorJob implements Jobby
 
       int attemptNumber = context.getTaskAttemptID().getId();
 
-      FileSystem fileSystem = FileSystem.get(context.getConfiguration());
-      Path indexBasePath = config.makeSegmentOutputPath(fileSystem, bucket);
+      Path indexBasePath = config.makeSegmentOutputPath(new DistributedFileSystem(), bucket);
       Path indexZipFilePath = new Path(indexBasePath, String.format("index.zip.%s", attemptNumber));
       final FileSystem infoFS = config.makeDescriptorInfoDir().getFileSystem(context.getConfiguration());
       final FileSystem outputFS = indexBasePath.getFileSystem(context.getConfiguration());
